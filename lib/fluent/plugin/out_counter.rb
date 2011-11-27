960fea6eae91e68a8bc1ffcda36d48067e549a9f
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-11-27 23:10:09 +0900

    add MongoStorage

--- lib/fluent/plugin/out_counter.rb
+++ lib/fluent/plugin/out_counter.rb
@@ -10,13 +10,19 @@ class Fluent::CounterOutput < Fluent::TimeSlicedOutput
   config_set_default :flush_interval, 1
   config_set_default :time_slice_wait, 1
 
+  config_param :mongo_host, :string, :default => 'localhost'
+  config_param :mongo_port, :integer, :default => 27017
+  config_param :mongo_database, :string, :default => 'counter'
+  config_param :mongo_collection, :string, :default => 'minutely'
+
   config_param :redis_host, :string, :default => 'localhost'
   config_param :redis_port, :integer, :default => 6379
-  config_param :redis_db, :string, :default => '0'
+  config_param :redis_database, :string, :default => '0'
 
   def initialize
     super
-    @storage = RedisStorage.new(@redis_host, @redis_port, @redis_db)
+    @storage = MongoStorage.new(@mongo_host, @mongo_port, @mongo_database, @mongo_collection)
+    # @storage = RedisStorage.new(@redis_host, @redis_port, @redis_database)
   end
 
   def configure(conf)
@@ -55,21 +61,62 @@ class Fluent::CounterOutput < Fluent::TimeSlicedOutput
     }
   end
 
-  class RedisStorage
+  class CounterStorage
+    def connect(&block)
+      conn = open
+      begin
+        yield conn
+      ensure
+        close(conn)
+      end
+    end
+
+    def open
+    end
+
+    def close
+    end
+  end
+
+  class MongoStorage < CounterStorage
+    def initialize(host, port, db, coll)
+      require 'mongo'
+      @host = host
+      @port = port
+      @db = db
+      @coll = coll
+    end
+
+    def open
+      db ||= Mongo::Connection.new(@host, @port).db(@db)
+      if db.collection_names.include?(@coll)
+        collection = db.collection(@coll)
+      else
+        arg = { :capped => true, :size => 10000000, :max => 10000000 }
+        collection = db.create_collection(@coll, arg)
+      end
+      collection
+    end
+
+    def close(conn)
+      conn.db.connection.close
+    end
+
+    def incr(conn, time_minute, key, value)
+      conn.update(
+        { :time => time_minute, :key => key },
+        { "$set" => { 'time' => time_minute },
+          "$inc" => { key => value } },
+        { :safe => true, :upsert => true })
+    end
+  end
+
+  class RedisStorage < CounterStorage
     def initialize(host, port, db)
       require 'redis'
       @host = host
       @port = port
       @db = db
-    end
-
-    def connect(&block)
-      conn = open
-      begin
-        conn.pipelined { yield conn }
-      ensure
-        close(conn)
-      end
     end
 
     def open
@@ -80,8 +127,8 @@ class Fluent::CounterOutput < Fluent::TimeSlicedOutput
       conn.quit
     end
 
-    def incr(conn, time, key, value)
-      conn.hincrby time, key, value
+    def incr(conn, time_minute, key, value)
+      conn.hincrby time_minute, key, value
     end
   end
 end
